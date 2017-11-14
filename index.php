b383eaed83e4c0316f3d48bf6a74fd8bc0b5b1ff
wang-renjie <wangrenjievip@163.com>
2017-11-15 01:01:03 +0800

    新增index,php文件夹

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WangRenjie
+ * Date: 2017/11/15
+ * Time: 0:55
+ */
+class IndexController {
+    public function indexAction()
+    {
+        echo '111111111111111111111111';
+    }
+
+    public function indexBaction()
+    {
+        echo '111111111111111111111111';
+    }
+    
+    
+    
+    
+}
